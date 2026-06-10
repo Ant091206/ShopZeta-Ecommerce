bcef13496c35c444cd68555a731113b000049853
Aryan <aaryanthakkar06@gmail.com>
2026-06-10 20:13:01 +0530

    Remove unused Tailwind configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './index.css'; // Make sure this matches your exact file name!import App from "./App";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
